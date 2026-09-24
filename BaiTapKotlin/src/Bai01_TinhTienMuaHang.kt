8a1824815254464fec6fc797e6535ed8b0639347
Tuấn Huê Lưu <luutuanhue23@gmail.com>
2026-09-24 11:52:18 +0700

    Buoi02 Bai01 tinh tien mua hang

---
+++ BaiTapKotlin/src/Bai01_TinhTienMuaHang.kt
@@ -0,0 +1,8 @@
+// Họ tên: Lưu Tuấn Huê, MSSV: 25810021
+fun main(){
+    var quantity: Int = 7
+    var price: Double = 23000.05
+    var total : Double = quantity.toDouble() * price
+    var amount : Double = total + (total * 0.8)
+    println("The final total amount is: $amount VND")
+}
