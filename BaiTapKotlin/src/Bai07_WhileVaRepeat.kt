246041759686d8cbac183827fa3cc82f34987b30
Tuấn Huê Lưu <luutuanhue23@gmail.com>
2026-09-24 14:44:31 +0700

    Buoi02 Bai07 dem nguoc va lap lai co dinh

---
+++ BaiTapKotlin/src/Bai07_WhileVaRepeat.kt
@@ -0,0 +1,14 @@
+// Họ tên: Lưu Tuấn Huê, MSSV: 25810021
+fun main() {
+    var i : Int = 10
+    //dùng while để có đk in ngược từ 10 về 1, count >0
+    while (i >0) {
+        println("Count $i")
+        i--
+    }
+
+    //dùng repeat lặp lại 5 lần cố định
+    repeat(5) {
+        println("Dòng cố định in 5 lần.")
+    }
+}
