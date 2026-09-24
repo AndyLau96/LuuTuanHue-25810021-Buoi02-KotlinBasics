7def95bb16d24be50fa15908c6c110a685fb1f9c
Tuấn Huê Lưu <luutuanhue23@gmail.com>
2026-09-24 13:31:49 +0700

    Buoi02 Bai03 tai khoan ngan hang don gian

---
+++ BaiTapKotlin/src/Bai03_TaiKhoanNganHang.kt
@@ -0,0 +1,11 @@
+// Họ tên: Lưu Tuấn Huê, MSSV: 25810021
+fun main() {
+    val soDuBanDau: Double = 5_000_000.00
+    var soDuHienTai: Double = soDuBanDau
+    //gd 1
+    soDuHienTai += 2_000_000.00
+    println("Số dư sau khi được cộng thêm là $soDuHienTai")
+    //gd 2
+    soDuHienTai -= 1_500_000.00
+    println("Số dư sau khi được rút tiền là $soDuHienTai")
+}
