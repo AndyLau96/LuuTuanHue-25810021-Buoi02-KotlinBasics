6320f93d001f5e3fbbca00ea05e6e33320b695df
Tuấn Huê Lưu <luutuanhue23@gmail.com>
2026-09-24 11:52:46 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ BaiTapKotlin/src/Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,8 @@
+// Họ tên: Lưu Tuấn Huê, MSSV: 25810021
+fun main(){
+    val fullName: String = "Lưu Tuấn Huê"
+    val birthYear: Int = 1996
+    val currentYear: Int = 2026
+    val currentAge: Int = currentYear - birthYear
+    println("Hello teacher, my name is $fullName. I am $currentAge years old.")
+}
