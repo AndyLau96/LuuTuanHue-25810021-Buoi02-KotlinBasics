0d645fc7733a5f9ad4091c4a14ebb608477b5b54
Tuấn Huê Lưu <luutuanhue23@gmail.com>
2026-09-24 13:34:37 +0700

    Buoi02 Bai03 tai khoan ngan hang don gian (da them chu thich val va var)

--- BaiTapKotlin/src/Bai03_TaiKhoanNganHang.kt
+++ BaiTapKotlin/src/Bai03_TaiKhoanNganHang.kt
@@ -1,6 +1,8 @@
 // Họ tên: Lưu Tuấn Huê, MSSV: 25810021
 fun main() {
+    //sử dụng val vì số dư ban đầu là cột mốc tham chiếu không thay đổi
     val soDuBanDau: Double = 5_000_000.00
+    //sử dụng var vì biến này sẽ thay đổi khi có giao dịch
     var soDuHienTai: Double = soDuBanDau
     //gd 1
     soDuHienTai += 2_000_000.00
